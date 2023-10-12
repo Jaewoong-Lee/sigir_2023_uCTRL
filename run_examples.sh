a1d74737ba420b56fa47b9d9b7ef74670baf7bba
AI-Recsys <31297938+Jaewoong-Lee@users.noreply.github.com>
2023-10-12 22:08:57 +0900

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -3,13 +3,13 @@
 python run_hyper.py --model=DirectAU --dataset=ml-1m --encoder=MF --params_file=search_hyper/directau.hyper --output_file=output_files/ml-1m/directau_mf --gpu_id=0 --eval=unbiased
 python run_hyper.py --model=DirectAU --dataset=ml-1m --encoder=LightGCN --params_file=search_hyper/directau.hyper --output_file=output_files/ml-1m/directau_lightgcn --gpu_id=0 --eval=unbiased
 
-python run_recbole.py --model=UCTRL --encoder=MF --dataset=ml-1m --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased &&
-python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=ml-1m --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=0 --eval=unbiased &&
-python run_recbole.py --model=UCTRL --encoder=MF --dataset=Gowalla2 --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased &&
-python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=Gowalla2 --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=0 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=MF --dataset=ml-1m --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased 
+python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=ml-1m --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=0 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=MF --dataset=Gowalla --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=Gowalla --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=0 --eval=unbiased
 
-python run_recbole.py --model=UCTRL --encoder=MF --dataset=Yelp2 --weight_decay=0 --gamma=1 --gpu_id=1 --eval=unbiased &&
-python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=Yelp2 --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=1 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=MF --dataset=Yelp --weight_decay=0 --gamma=1 --gpu_id=1 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=LightGCN --dataset=Yelp --weight_decay=0 --gamma=1 --n_layers=1 --gpu_id=1 --eval=unbiased
 
 python run_recbole.py --model=UCTRL --encoder=MF --dataset=ml-1m --train_batch_size=512 --learning_rate=5e-3 --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased
-python run_recbole.py --model=UCTRL --encoder=MF --dataset=Gowalla2  --train_batch_size=512 --learning_rate=5e-3 --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased
+python run_recbole.py --model=UCTRL --encoder=MF --dataset=Gowalla  --train_batch_size=512 --learning_rate=5e-3 --weight_decay=0 --gamma=1 --gpu_id=0 --eval=unbiased
